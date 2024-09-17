905a6c9a477c54f8f94dcb4483f6da336bc9f2ad
Dijets <davidhill0173@gmail.com>
2024-09-17 14:22:29 +0100

    Update Footer.tsx

--- src/ui/footer/Footer.tsx
+++ src/ui/footer/Footer.tsx
@@ -152,14 +152,3 @@ export async function Footer() {
 		</footer>
 	);
 }
-
-function TwitterIcon(props: SVGAttributes<SVGSVGElement>) {
-	return (
-		<svg {...props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 596 596" fill="none">
-			<path
-				fill="#fff"
-				d="m1 19 230 307L0 577h52l203-219 164 219h177L353 252 568 19h-52L329 221 179 19H1Zm77 38h82l359 481h-81L78 57Z"
-			/>
-		</svg>
-	);
-}
